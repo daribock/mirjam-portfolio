070088b4c93e3780c6588db1fa9c04f8e99ef124
Darius Kletter <darikletter@gmail.com>
2025-02-25 21:55:16 +0100

    feat: smaller fade

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -37,7 +37,7 @@ export const SingleProject = ({ project }: { project: Project }) => {
           width="1000"
           className="rounded-md object-contain"
         />
-        <div className="absolute bottom-0 bg-white h-40 w-full [mask-image:linear-gradient(to_bottom,transparent,white)]" />
+        <div className="absolute bottom-0 bg-white h-10 lg:h-40 w-full [mask-image:linear-gradient(to_bottom,transparent,white)]" />
       </motion.div>
       {project.images && (
         <div className="flex flex-row justify-center my-8 flex-wrap">
